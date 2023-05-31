8389115598f0d1b3beb51cdd8156c26696ac18dd
GotoRen <r0719en@pluslab.org>
2023-05-31 17:49:04 +0900

    [Fix] scenario

--- src/prometheus/install.sh
+++ src/prometheus/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# kubectl create namespace monitoring
+kubectl create namespace monitoring
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm upgrade prometheus prometheus-community/prometheus \
